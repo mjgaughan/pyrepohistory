3e39cf0f64c50c87a487fb1e5a2b617121e782d8
mgaughan <mgaughan@proton.me>
2024-12-10 10:23:22 -0600

    branching wip off to the side for now, can't figure

--- src/lib/collating_prs.py
+++ src/lib/collating_prs.py
@@ -1,4 +1,9 @@
 import git
+import os
+
+import logging
+logging.basicConfig()
+logging.root.setLevel(logging.INFO)
 
 def get_prs(repo_path):
     """
@@ -8,13 +13,12 @@ def get_prs(repo_path):
         prs (array of dicts): array of dictionary objects with prs, opened and closed
     """
     repo = git.Repo(repo_path)
+    os.environ["GIT_PYTHON_TRACE"] = "full"
     repo.remote().fetch('refs/pull/*/head:refs/pull/*/head')
-    results = repo.remote().fetch('refs/pull/*/merge:refs/pull/*/merge')
-
+    repo.remote().fetch('refs/pull/*/merge:refs/pull/*/merge')
     pr_refs = [ref for ref in repo.refs if ref.name.startswith('refs/pull/')]
+
     print(pr_refs)
-    for ref in pr_refs:
-        print(ref)
     '''
     #repo.git.fetch('origin', 'refs/pull/*/head:refs/pull/*/head')
     #repo.git.fetch('origin', 'refs/pull/*/merge:refs/pull/*/merge')
@@ -28,5 +32,5 @@ def get_prs(repo_path):
     '''
 
 if __name__ == "__main__":
-    repo_path = '../../../hipcheck'
+    repo_path = '../../../gawain'
     get_prs(repo_path)
