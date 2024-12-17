f5e9032049918605dd589457381475663b562864
mgaughan <mgaughan@proton.me>
2024-12-17 09:33:21 -1000

    new build and package

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup( name ="pyrepohistory", 
-       version = "0.0.10",
+       version = "0.0.11",
        author = "Matt Gaughan",
        long_description = long_description, 
        long_description_content_type="text/markdown", 
